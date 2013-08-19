9d7e97d05662a51e6878dcc458db806fb0e88efa
ka <ka.kaosf@gmail.com>
2013-08-19 22:24:29 +0900

    display all of "request"

--- src/hello_world/core.clj
+++ src/hello_world/core.clj
@@ -3,4 +3,7 @@
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "Hello World"})
+   :body (str
+     "<ul>\n"
+     (apply str (map #(str "<li>" % "</li>\n") (seq request)))
+     "</ul>\n")})
