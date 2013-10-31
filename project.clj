07942cd7c995cee807561602de3989dc3ef1df37
ka <ka.kaosf@gmail.com>
2013-10-31 19:59:04 +0900

    use hiccup 1.0.4

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-core "1.2.1"]
-                 [ring/ring-jetty-adapter "1.2.1"]])
+                 [ring/ring-jetty-adapter "1.2.1"]
+                 [hiccup "1.0.4"]])
