469ac126ef30224be125050c4de7fa1cb2b42633
ka <ka.kaosf@gmail.com>
2013-10-31 19:59:59 +0900

    add hiccup experimental code

--- src/hello_world/core.clj
+++ src/hello_world/core.clj
@@ -1,10 +1,13 @@
 (ns hello-world.core
   (:use (ring.adapter jetty)))
 
+(use 'hiccup.core)
+
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body (str
+     (html [:p "paragraph"])
      "<ul>\n"
      (apply str (map #(str "<li>" % "</li>\n") (seq request)))
      "</ul>\n")})
